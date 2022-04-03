2cf09e8da295751ba2390dff466ce3b624e9ee2c
Sadyrinnik <82495558+sadnick@users.noreply.github.com>
2022-04-03 22:39:16 +0300

    Add files via upload

---
+++ MVVM/MVVM/DelegateCommand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MVVM
+{
+    public class DelegateCommand<T>
+    {
+        private Action<object> p;
+
+        public DelegateCommand(Action<object> p)
+        {
+            this.p = p;
+        }
+    }
+}
---
+++ MVVM/MVVM/MainVM.cs
@@ -0,0 +1,44 @@
+﻿using Prism.Commands;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVM
+{
+    class MainVM : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private int _number1;
+        public int Number1
+        {
+            get { return _number1; }
+            set
+            {
+                _number1 = value;
+                OnPropertyChanged("Number3"); // уведомление View о том, что изменилась сумма
+            }
+        }
+
+        private int _number2;
+        public int Number2
+        {
+            get { return _number2; }
+            set { _number2 = value; OnPropertyChanged("Number3"); }
+        }
+
+        public int Number3 => MathFuncs.GetSumOf(Number1, Number2);
+        static class MathFuncs
+        {
+            public static int GetSumOf(int a, int b) => a + b;
+        }
+    }
+}
